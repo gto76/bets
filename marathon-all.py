eb2fb112877de7e4cb16453c81887f1730371c5d
Jure Sorn <sornjure@gmail.com>
2015-12-04 19:16:09 +0100

    Marathon

--- marathon-all.py
+++ marathon-all.py
@@ -3,40 +3,28 @@
 # Usage: favbet.py 
 # https://www.favbet.com/en/bets/#tour=17745&event=4198442
 
+import os
+import sys
+import json
+
 from contextlib import closing
 from selenium.webdriver import Firefox # pip install selenium
 from selenium.webdriver import Chrome # pip install selenium
 from selenium.webdriver.support.ui import WebDriverWait
-
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
-import os
-import sys
-
-import json
-
 from bs4 import BeautifulSoup
 
 def selenium():
-  browser = Firefox()
-  browser.wait = WebDriverWait(browser, 2)
-  browser.get('https://www.marathonbet.com/hr/betting/Basketball/NBA/')
-
-  WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='member-name nowrap ']")))
-
-  element = browser.find_element_by_xpath("//div[@class='member-name nowrap ']")
-  element.click()
-
-  WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((By.XPATH, "//table[@class='table-shortcuts-menu']")))
-
-  bla = browser.page_source
-  return bla
-  #print(bla)
-
+  with closing(Firefox()) as browser:
+    browser.get('https://www.marathonbet.com/hr/betting/Basketball/NBA/')
+    WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='member-name nowrap ']")))
+    browser.find_element_by_xpath("//div[@class='member-name nowrap ']").click()
+    WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((By.XPATH, "//table[@class='table-shortcuts-menu']")))
+    return browser.page_source
+    
 def main():
-  #html = open("marathonbet.html", encoding='utf8')
   html = selenium()
   soup = BeautifulSoup(html, "html.parser")
 
