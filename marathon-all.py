3006cf53a0c0d4a12ba2d6f14efa84cbf1073e41
Jure Sorn <sornjure@gmail.com>
2015-12-05 15:44:39 +0100

    Working on marathon

--- marathon-all.py
+++ marathon-all.py
@@ -17,8 +17,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-BOOKIE_NAME =
-BOOKIE_URL = 
+BOOKIE_NAME = "Marathonbet"
+BOOKIE_URL = "https://www.marathonbet.com/hr/betting/Basketball/NBA/"
 
 class Player:
   pass
@@ -27,13 +27,18 @@ def main():
   dates, htmls = selenium()
   players = []
   for date, html in zip(dates, htmls):
+    date = cleanDate(date)
     players.extend(getPlayers(html, date))
   for p in players:
     pprint (vars(p))
 
+def cleanDate(date):
+  date = re.sub("^\n *", "", date)
+  return re.sub("\n *$", "", date)
+
 def selenium():
   with closing(Firefox()) as browser:
-    browser.get('https://www.marathonbet.com/hr/betting/Basketball/NBA/')
+    browser.get(BOOKIE_URL)
     WebDriverWait(browser, timeout=10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='member-name nowrap ']")))
     dates = getDates(browser.page_source)
     elements = browser.find_elements_by_xpath("//div[@class='member-name nowrap ']")
@@ -60,14 +65,23 @@ def getPlayers(html, date):
   for a, b in pairwise(pl):
     player = Player()
     respA = json.loads(a["data-sel"])
-    player.player_name = respA["mn"]
-    player.player_total = respA["sn"]
+    player.player_name = cleanName(respA["mn"])
+    player.player_total = cleanPoints(respA["sn"])
     player.under = respA["epr"]
     respB = json.loads(b["data-sel"])
     player.over = respB["epr"]
     player.start_time = date
+    player.bookie_name = BOOKIE_NAME
+    player.bookie_url = BOOKIE_URL
     players.append(player)
   return players
+
+def cleanName(name):
+  name = re.sub("Points( ", "", name)
+  return re.sub("[,)]", "", name)
+
+def cleanPoints(points):
+  return re.sub("^[^0-9]*", "", points)
 
 def pairwise(t):
     it = iter(t)
